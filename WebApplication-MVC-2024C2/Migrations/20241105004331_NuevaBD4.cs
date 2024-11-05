f0b4365c475ba8f5d4d97390e17981d5371defe7
alejo <alejohuerga@gmail.com>
2024-11-04 22:08:59 -0300

    Reapply "Version Estable 1.2.1"
    This reverts commit 0bf9083f70b229b2dfc3eb040dc5147404441e36.

---
+++ WebApplication-MVC-2024C2/Migrations/20241105004331_NuevaBD4.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication_MVC_2024C2.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaBD4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CantButacas",
+                table: "Peliculas",
+                newName: "Butacas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Butacas",
+                table: "Peliculas",
+                newName: "CantButacas");
+        }
+    }
+}
