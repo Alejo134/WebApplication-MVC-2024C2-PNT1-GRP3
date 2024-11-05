c7fda7e6aac202cd6eabd289d8fb38d8ea5a179c
Ivan <ivanschutt@hotmail.com>
2024-11-04 22:13:20 -0300

    Version Definitiva 1.3

---
+++ WebApplication-MVC-2024C2/Migrations/20241105011011_NuevaDB8.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication_MVC_2024C2.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaDB8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
