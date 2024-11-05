fa6dedc93cbc29b36dd5e6aa4b88e8940eb664be
Ivan Liscovsky <liscovsky.ivan@hotmail.com>
2024-11-04 21:47:33 -0300

    Version Estable 1.2.1

--- WebApplication-MVC-2024C2/Migrations/20241105004331_NuevaBD4.Designer.cs
+++ WebApplication-MVC-2024C2/Migrations/20241105004331_NuevaBD4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication_MVC_2024C2.Context;
 
@@ -11,9 +12,11 @@ using WebApplication_MVC_2024C2.Context;
 namespace WebApplication_MVC_2024C2.Migrations
 {
     [DbContext(typeof(CineDataBaseContext))]
-    partial class CineDataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241105004331_NuevaBD4")]
+    partial class NuevaBD4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
