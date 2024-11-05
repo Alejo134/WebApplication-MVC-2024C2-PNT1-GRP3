beb8b00abf3d117309ac0eed2339145c7fdf0b32
Ivan <ivanschutt@hotmail.com>
2024-11-04 22:50:40 -0300

    Version Estable Con Peliculas Creadas

--- WebApplication-MVC-2024C2/Models/Pelicula.cs
+++ WebApplication-MVC-2024C2/Models/Pelicula.cs
@@ -9,7 +9,6 @@ namespace WebApplication_MVC_2024C2.Models
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
-        public string PortadaPelicula { get; set; }
         public string Clasificacion { get; set; }
         public int NroDeSala { get; set; }
         public DateTime Fecha { get; set; }
