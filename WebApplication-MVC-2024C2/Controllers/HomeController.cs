e003bed8d50e2c5cbd19c837630f999052fbf867
Brianquindi1 <brianquindi1@gmail.com>
2024-11-28 11:00:32 -0300

    Arregrando los problemas de la cartelera

--- WebApplication-MVC-2024C2/Controllers/HomeController.cs
+++ WebApplication-MVC-2024C2/Controllers/HomeController.cs
@@ -1,24 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebApplication_MVC_2024C2.Context;
 using WebApplication_MVC_2024C2.Models;
 
 namespace WebApplication_MVC_2024C2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly PeliculasController _peliculasController;
+        //  private readonly ILogger<HomeController> _logger;
+        //  private readonly PeliculasController _peliculasController;
+        private readonly CineDataBaseContext _context;
 
 
-        public HomeController(ILogger<HomeController> logger, PeliculasController peliculasController)
+        public HomeController(/*ILogger<HomeController> logger*/CineDataBaseContext context)
         {
-            _peliculasController = peliculasController;
-            _logger = logger;
+            _context = context;
+          //  _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var peliculas = _peliculasController.Index(); // Llama al método Index de PeliculasController
+            /*  var peliculas = _peliculasController.Index(); // Llama al método Index de PeliculasController
+              ViewBag.Cartelera = peliculas;*/
+            var peliculas = await _context.Peliculas.ToListAsync();
             ViewBag.Cartelera = peliculas;
             return View();
         }
