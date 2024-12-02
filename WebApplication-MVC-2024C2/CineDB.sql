bfbc248fc6399f65e54956cc5d27132ff56eddc5
alejo <alejohuerga@gmail.com>
2024-12-02 18:04:37 -0300

    Cambios de Pelicula - Nuevo ENUM para las clasificaciones.
    Actualizacion de la base de datos, cambio el tipo de clasificacion de String a INT

--- WebApplication-MVC-2024C2/CineDB.sql
+++ WebApplication-MVC-2024C2/CineDB.sql
@@ -1,6 +1,6 @@
 USE [master]
 GO
-/****** Object:  Database [CineDB]    Script Date: 17/11/2024 15:29:31 ******/
+/****** Object:  Database [CineDB]    Script Date: 2/12/2024 17:58:35 ******/
 CREATE DATABASE [CineDB]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -82,7 +82,7 @@ ALTER DATABASE [CineDB] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_PO
 GO
 USE [CineDB]
 GO
-/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 17/11/2024 15:29:31 ******/
+/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 2/12/2024 17:58:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -96,7 +96,7 @@ CREATE TABLE [dbo].[__EFMigrationsHistory](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[NuevoUsuario]    Script Date: 17/11/2024 15:29:31 ******/
+/****** Object:  Table [dbo].[NuevoUsuario]    Script Date: 2/12/2024 17:58:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -113,7 +113,7 @@ CREATE TABLE [dbo].[NuevoUsuario](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Peliculas]    Script Date: 17/11/2024 15:29:31 ******/
+/****** Object:  Table [dbo].[Peliculas]    Script Date: 2/12/2024 17:58:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -122,7 +122,7 @@ CREATE TABLE [dbo].[Peliculas](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Titulo] [nvarchar](max) NOT NULL,
 	[Descripcion] [nvarchar](max) NOT NULL,
-	[Clasificacion] [nvarchar](max) NOT NULL,
+	[Clasificacion] [int] NULL,
 	[NroDeSala] [int] NOT NULL,
 	[Fecha] [datetime2](7) NOT NULL,
 	[Precio] [float] NOT NULL,
@@ -133,7 +133,7 @@ CREATE TABLE [dbo].[Peliculas](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Ventas]    Script Date: 17/11/2024 15:29:31 ******/
+/****** Object:  Table [dbo].[Ventas]    Script Date: 2/12/2024 17:58:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -151,44 +151,6 @@ CREATE TABLE [dbo].[Ventas](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241022011142_inicial', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241105002619_NuevaBD', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241105003228_NuevaBD2', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241105003457_NuevaBD3', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241105011011_NuevaDB8', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241105013436_NuevaDB9', N'8.0.10')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241113234442_nuevousuario', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241113235035_nuevoUsuario2', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241114131848_Venta1', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241114133651_Venta2', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115020316_NuevaVenta', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115023834_NuevaVenta2', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115030100_Ventas3', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115191920_Ventas4', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115200235_Ventas5', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241115204233_Ventas6', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117140421_VentaConIDUsuario1', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117143309_AgregadoDePuntosAUsuario1', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117144610_AgregadoDePuntosAUsuario2', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117145934_AgregadoDePuntosAUsuario3', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117173959_AgregadoDePuntosAUsuario4', N'8.0.11')
-INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20241117182418_AgregadoDePuntosAUsuario5', N'8.0.11')
-GO
-SET IDENTITY_INSERT [dbo].[NuevoUsuario] ON 
-
-INSERT [dbo].[NuevoUsuario] ([Id], [Usuario], [Contrasenia], [Email], [Puntos]) VALUES (7, N'UsuarioConPuntos', N'123456', N'usuarioConPuntos@gmail.com', 1000)
-SET IDENTITY_INSERT [dbo].[NuevoUsuario] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Peliculas] ON 
-
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (1, N'Spider-Man (2002)', N'Spider-Man (2002) es una película de superhéroes dirigida por Sam Raimi, basada en el personaje de cómic del mismo nombre creado por Stan Lee y Steve Ditko para Marvel Comics. Es la primera entrega de una trilogía de Spider-Man protagonizada por Tobey Maguire como Peter Parker / Spider-Man.', N'+12', 1, CAST(N'2025-01-01T18:30:00.0000000' AS DateTime2), 10000, 15)
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (2, N'Spider-Man (2002)', N'Spider-Man (2002) es una película de superhéroes dirigida por Sam Raimi, basada en el personaje de cómic del mismo nombre creado por Stan Lee y Steve Ditko para Marvel Comics. Es la primera entrega de una trilogía de Spider-Man protagonizada por Tobey Maguire como Peter Parker / Spider-Man.', N'+12', 1, CAST(N'2025-01-08T18:30:00.0000000' AS DateTime2), 10000, 19)
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (3, N'Spider-Man 2 (2004)', N'Spider-Man 2 (2004) es la secuela de Spider-Man (2002), dirigida nuevamente por Sam Raimi y protagonizada por Tobey Maguire como Peter Parker / Spider-Man. La película sigue la evolución de Peter Parker mientras lucha por equilibrar su vida como superhéroe y como persona, enfrentando nuevos desafíos tanto emocionales como físicos.', N'+12', 2, CAST(N'2025-01-01T18:30:00.0000000' AS DateTime2), 10000, 19)
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (4, N'Spider-Man 2 (2004)', N'Spider-Man 2 (2004) es la secuela de Spider-Man (2002), dirigida nuevamente por Sam Raimi y protagonizada por Tobey Maguire como Peter Parker / Spider-Man. La película sigue la evolución de Peter Parker mientras lucha por equilibrar su vida como superhéroe y como persona, enfrentando nuevos desafíos tanto emocionales como físicos.', N'+12', 2, CAST(N'2025-01-08T18:30:00.0000000' AS DateTime2), 10000, 20)
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (5, N'Spider-Man 3 (2007)', N'Spider-Man 3 (2007) es la tercera y última entrega de la trilogía dirigida por Sam Raimi sobre el icónico superhéroe de Marvel. Aunque esta película continúa explorando los dilemas personales de Peter Parker / Spider-Man, se distingue por su tono más oscuro y la inclusión de varios villanos, lo que llevó a una trama más compleja y, en algunos aspectos, más dividida entre acción y desarrollo de personajes.', N'+12', 3, CAST(N'2025-01-01T18:30:00.0000000' AS DateTime2), 10000, 20)
-INSERT [dbo].[Peliculas] ([Id], [Titulo], [Descripcion], [Clasificacion], [NroDeSala], [Fecha], [Precio], [CantButacas]) VALUES (6, N'Spider-Man 3 (2007)', N'Spider-Man 3 (2007) es la tercera y última entrega de la trilogía dirigida por Sam Raimi sobre el icónico superhéroe de Marvel. Aunque esta película continúa explorando los dilemas personales de Peter Parker / Spider-Man, se distingue por su tono más oscuro y la inclusión de varios villanos, lo que llevó a una trama más compleja y, en algunos aspectos, más dividida entre acción y desarrollo de personajes.', N'+12', 3, CAST(N'2025-01-08T18:30:00.0000000' AS DateTime2), 10000, 20)
-SET IDENTITY_INSERT [dbo].[Peliculas] OFF
 GO
 ALTER TABLE [dbo].[NuevoUsuario] ADD  DEFAULT ((0)) FOR [Puntos]
 GO
---
+++ WebApplication-MVC-2024C2/Models/Clasificacion.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication_MVC_2024C2.Models
+{
+    public enum Clasificacion
+    {
+        ATP = 0,
+        PG13 = 1,
+        PG16 = 2,
+        PG18 = 3
+    }
+}
